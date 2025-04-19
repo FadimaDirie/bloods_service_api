c0c28fcb7bac0fcfe1447013992f98c7e853fb36
FadimaDirie <fadnaaden05@gmail.com>
2025-04-19 18:12:10 +0300

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const UserRouter = require('./Router/UserRouter');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();

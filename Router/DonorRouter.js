809c72ee487558a3679a5630140b38f5e6f4e56e
FadimaDirie <fadnaaden05@gmail.com>
2025-05-20 12:58:30 +0300

    update

---
+++ Router/DonorRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Donor = require('../models/donor');
+
+// POST /api/donors
+DonorRouter.post('/donors', async (req, res) => {
+  try {
+    const newDonor = new Donor(req.body);
+    const saved = await newDonor.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// GET /api/donors
+DonorRouter.get('/donors', async (req, res) => {
+  try {
+    const allDonors = await Donor.find();
+    res.json(allDonors);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const UserRouter = require('./Router/UserRouter');
+const DonorRouter = require('./Router/DonorRouter')
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs'); 
@@ -25,6 +26,7 @@ app.use('/uploads', express.static(uploadsDir));
 
 
 app.use('/api/user', UserRouter);
+app.use('/api/donors', DonorRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

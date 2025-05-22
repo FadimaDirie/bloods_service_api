55ad22e22e79c61560664a7a9ce713dd415522c7
FadimaDirie <fadnaaden05@gmail.com>
2025-05-22 22:24:51 +0300

    update

--- models/User.js
+++ models/User.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   fullName: { type: String, unique: true,required: true },
   email: { type: String, required: true, unique: true },
-  age: { type: Number, unique: true,required: true  },
+  age: { type: Number},
   phone: { type: String, unique: true ,required: true },
-  location: { type: String, unique: true ,required: true },
-  bloodType: { type: String, unique: true ,required: true },
-  username: { type: String, unique: true ,required: true },
-  password: { type: String, unique: true ,required: true },
+  location: { type: String},
+  bloodType: { type: String},
+  username: { type: String, unique: true},
+  password: { type: String},
   profilePic: { type: String, unique: true,required: true  },
   roles: {
     isDonor: { type: Boolean, default: false },

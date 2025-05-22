5bbdda7286ae8627dee6bea7ccd25b44882fa314
FadimaDirie <fadnaaden05@gmail.com>
2025-05-22 22:38:04 +0300

    update

--- models/User.js
+++ models/User.js
@@ -1,20 +1,19 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  fullName: { type: String, unique: true,required: true },
+  fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  age: { type: Number},
-  phone: { type: String, unique: true ,required: true },
-  location: { type: String},
-  bloodType: { type: String},
-  username: { type: String, unique: true},
-  password: { type: String},
-  profilePic: { type: String, unique: true,required: true  },
+  age: { type: Number },
+  phone: { type: String, required: true, unique: true },
+  location: { type: String },
+  bloodType: { type: String },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePic: { type: String }, // no unique, no required
   roles: {
     isDonor: { type: Boolean, default: false },
     isRequester: { type: Boolean, default: false }
   }
 }, { timestamps: true });
-
 
 module.exports = mongoose.model('User', UserSchema);

d3d2fa15b8c1edab1cde2cadfd605725486a7151
FadimaDirie <fadnaaden05@gmail.com>
2025-05-25 18:45:47 +0300

    Firebase env var support

--- convertKey.js
+++ convertKey.js
@@ -1,9 +1,9 @@
+// convertKey.js
 const fs = require('fs');
-const path = require('path');
 
-const jsonPath = path.join(__dirname, './uploads/bloodlife-bc66b-firebase-adminsdk-fbsvc-86f8e3f36e.json');
-const data = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'));
+const raw = fs.readFileSync('./uploads/bloodlife-bc66b-firebase-adminsdk-fbsvc-86f8e3f36e.json', 'utf8');
+const parsed = JSON.parse(raw);
 
-data.private_key = data.private_key.replace(/\n/g, '\\n');
+parsed.private_key = parsed.private_key.replace(/\n/g, '\\n');
 
-console.log(JSON.stringify(data, null, 2)); // Copy this output only!
+console.log(JSON.stringify(parsed, null, 2));
--- firebase.js
+++ firebase.js
@@ -1,10 +1,8 @@
-// firebase.js
 const admin = require('firebase-admin');
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;

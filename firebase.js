419695e2e73e2c6901f7b15434f8126bc873f051
FadimaDirie <fadnaaden05@gmail.com>
2025-05-25 10:35:06 +0300

    Firebase env var support

--- firebase.js
+++ firebase.js
@@ -1,12 +1,5 @@
 const admin = require('firebase-admin');
-
-let serviceAccount;
-
-if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-} else {
-  const serviceAccount = require('./uploads/bloodlife-bc66b-firebase-adminsdk-fbsvc-<...>.json'); // write full name // fallback for local dev
-}
+const serviceAccount = require('./uploads/bloodlife-bc66b-firebase-adminsdk-fbsvc-<...>.json'); // write full name
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

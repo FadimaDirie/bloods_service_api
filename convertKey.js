d9189e1ca4cfb66a0a0c45657cc063e980aa652b
FadimaDirie <fadnaaden05@gmail.com>
2025-05-25 18:03:59 +0300

    Firebase env var support

--- convertKey.js
+++ convertKey.js
@@ -1,15 +1,9 @@
-// convertKey.js
-
 const fs = require('fs');
+const path = require('path');
 
-// 1. Akhri file-ka JSON
-const rawData = fs.readFileSync('./uploads/bloodlife-bc66b-firebase-adminsdk-fbsvc-86f8e3f36e.json', 'utf8');
+const jsonPath = path.join(__dirname, './uploads/bloodlife-bc66b-firebase-adminsdk-fbsvc-86f8e3f36e.json');
+const data = JSON.parse(fs.readFileSync(jsonPath, 'utf-8'));
 
-// 2. Parse JSON-ka
-const data = JSON.parse(rawData);
-
-// 3. Badal \n line breaks ee private_key ku jira
 data.private_key = data.private_key.replace(/\n/g, '\\n');
 
-// 4. Daabac output-ka saxda ah (JSON string oo diyaarsan)
-console.log(JSON.stringify(data, null, 2));
+console.log(JSON.stringify(data, null, 2)); // Copy this output only!

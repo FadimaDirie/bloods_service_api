672eabaac3e4cdb3bc41990b733b0c4cd79ff2f6
FadimaDirie <fadnaaden05@gmail.com>
2025-05-26 16:25:51 +0300

    Firebase env var support

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
 const admin = require('firebase-admin');
+
+// Parse the JSON string from env
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// Replace escaped \n with real newlines in private_key
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
-module.exports = admin;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});

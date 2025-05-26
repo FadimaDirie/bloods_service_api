a6662e9b92a894104f580c75ef4fd31e5c1f6847
FadimaDirie <fadnaaden05@gmail.com>
2025-05-26 16:13:58 +0300

    Firebase env var support

--- firebase.js
+++ firebase.js
@@ -1,8 +1,5 @@
-const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
+const admin = require("firebase-admin");
+const serviceAccount = require("./uploads/bloodlife-bc66b-firebase-adminsdk-fbsvc-86f8e3f36e.json")
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
-module.exports = admin;

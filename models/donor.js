b296375f838b91eeff129a9ea9114f4a48525895
FadimaDirie <fadnaaden05@gmail.com>
2025-05-28 02:36:30 +0300

    Firebase env var support

--- models/donor.js
+++ models/donor.js
@@ -7,9 +7,9 @@ const donorSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   bloodGroup: { type: String, required: true },
   availability: { type: String, required: true },
-  lastDonationDate: { type: Date, required: true },
-  weight: { type: Number, required: true },
-  age: { type: Number, required: true }
+  lastDonationDate: { type: Date},
+  weight: { type: Number },
+  age: { type: Number}
 }, { timestamps: true });
 
 module.exports = mongoose.model('Donor', donorSchema);

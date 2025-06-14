6595aee725b79568479e559633dfe6094cf68fb2
E-Maryan-M-APP <maryama@tabaarakict.org>
2025-06-14 15:47:08 +0300

    update

--- Router/orderRoutes.js
+++ Router/orderRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
+const UserRouter = require('./routes/UserRouter'); // adjust path if needed
 
 router.post('/orders', orderController.createOrder);
 router.get('/myorders/:userId', orderController.getMyOrders);
-const UserRouter = require('./Router/UserRouter'); // adjust path if needed
 router.put('/orders/:orderId/status', orderController.updateOrderStatus);
 
 module.exports = router;

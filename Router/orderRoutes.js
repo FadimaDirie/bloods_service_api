15720272204c15923d2952448dcd8ec688a8bad5
FadimaDirie <fadnaaden05@gmail.com>
2025-06-14 16:30:20 +0300

    fix: added UserRouter or corrected path

--- Router/orderRoutes.js
+++ Router/orderRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
-const UserRouter = require('./routes/UserRouter'); // adjust path if needed
+const UserRouter = require('./UserRouter'); // adjust path if needed
 
 router.post('/orders', orderController.createOrder);
 router.get('/myorders/:userId', orderController.getMyOrders);
